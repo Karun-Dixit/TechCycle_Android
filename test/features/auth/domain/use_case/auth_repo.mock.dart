cf9ecc09f0808b1c0efc8dcf125b70c1b4137f86
Karun-Dixit <karundixit5@gmail.com>
2025-02-07 17:12:48 +0545

    mock repo

---
+++ test/features/auth/domain/use_case/auth_repo.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:sprint1/features/auth/domain/repository/auth_repository.dart';
+
+class AuthRepoMock extends Mock implements IAuthRepository {}
