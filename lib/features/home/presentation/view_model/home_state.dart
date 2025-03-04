cc97f729b86dd39d456280cd3a64013505da0cdc
Karun-Dixit <karundixit5@gmail.com>
2025-03-04 20:27:45 +0545

    dark mode done

--- lib/features/home/presentation/view_model/home_state.dart
+++ lib/features/home/presentation/view_model/home_state.dart
@@ -1,41 +1,37 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeState extends Equatable {
   final int selectedIndex;
   final List<Widget> views;
+  final ThemeMode themeMode; // Added for dark mode
 
   const HomeState({
     required this.selectedIndex,
     required this.views,
+    required this.themeMode,
   });
 
-  // Initial state
   static HomeState initial() {
     return const HomeState(
-      selectedIndex: 0,
-      views: [
-        Center(
-          child: Text('Dashboard'),
-        ),
-        Center(
-          child: Text('Account'),
-        ),
-      ],
+      selectedIndex: 1, // Match your default Shop tab
+      views: [], // Empty since not used
+      themeMode: ThemeMode.light, // Default to light mode
     );
   }
 
   HomeState copyWith({
     int? selectedIndex,
     List<Widget>? views,
+    ThemeMode? themeMode,
   }) {
     return HomeState(
       selectedIndex: selectedIndex ?? this.selectedIndex,
       views: views ?? this.views,
+      themeMode: themeMode ?? this.themeMode,
     );
   }
 
   @override
-  List<Object?> get props => [selectedIndex, views];
+  List<Object?> get props => [selectedIndex, views, themeMode];
 }
