4a7e44c0d05e634344aab559d2c6a22c5bec9ff6
Karun-Dixit <karundixit5@gmail.com>
2025-03-04 16:29:27 +0545

    update

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,20 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sprint1/app/di/di.dart';
 import 'package:sprint1/features/auth/presentation/view/login_view.dart';
-
-import '../features/auth/presentation/view_model/login/login_bloc.dart';
+import 'package:sprint1/features/auth/presentation/view_model/login/login_bloc.dart';
+import 'package:sprint1/features/home/presentation/bloc/home_bloc.dart';
+import 'package:sprint1/features/home/presentation/view/bottom_navigation_bar.dart';
+import 'package:sprint1/features/home/presentation/view_model/home_cubit.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Tech Cycle',
-      home: BlocProvider.value(
-        value: getIt<LoginBloc>(),
-        child: LoginView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider.value(value: getIt<LoginBloc>()),
+        BlocProvider.value(value: getIt<HomeBloc>()),
+        BlocProvider(create: (_) => HomeCubit()), // HomeCubit not in getIt, so created here
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Tech Cycle',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          scaffoldBackgroundColor: Colors.grey[100],
+        ),
+        home: LoginView(),
+        routes: {
+          '/home': (context) => const CustomBottomNavigationBar(),
+        },
       ),
     );
   }
