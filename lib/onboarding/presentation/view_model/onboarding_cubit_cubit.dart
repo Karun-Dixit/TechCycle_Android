768fd5c417b4f04cb8bfbd19daa49262351f8da0
Karun-Dixit <karundixit5@gmail.com>
2025-03-05 11:12:34 +0545

    deleted

--- lib/onboarding/presentation/view_model/onboarding_cubit_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'onboarding_cubit_state.dart';
-
-class OnboardingCubitCubit extends Cubit<OnboardingCubitState> {
-  OnboardingCubitCubit() : super(OnboardingCubitInitial());
-}
