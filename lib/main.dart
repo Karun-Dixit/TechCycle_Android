5dd04b83411f0eab1b942f3c9c40ec27a7ef32eb
Karun-Dixit <karundixit5@gmail.com>
2025-03-05 13:53:16 +0545

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:sprint1/features/home/data/model/product.dart';
 import 'package:sprint1/app/app.dart';
 import 'package:sprint1/app/di/di.dart';
-import 'package:sprint1/core/network/hive_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await HiveService.init();
-
-  // await HiveService().clearStudentBox();
-
-  await initDependencies();
-
-  runApp(
-    const App(),
-  );
+  await Hive.initFlutter();
+  Hive.registerAdapter(ProductAdapter()); // Register your model adapter
+  await Hive.openBox<Product>('productsBox'); // Open a Hive box for products
+  await initDependencies(); // Initialize dependencies before running the app
+  runApp(const App());
 }
